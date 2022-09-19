ba0ad0532e016d388e0a8b5d1158e70024a5676b
KuroShiroe <puglisichris@gmail.com>
2022-09-19 18:38:50 -0400

    Flixster Part 2

---
+++ Flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flixster
+//
+//  Created by Priscilla Puglisi on 9/19/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
